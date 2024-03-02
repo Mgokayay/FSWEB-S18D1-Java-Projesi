fee881a1564f198b58e1b2c1ad8ba98518cbc036
Mgokayay <aymustafagokay@gmail.com>
2024-03-02 21:42:31 +0300

    project completed

---
+++ src/main/java/com/workintech/s18d1/entity/Burger.java
@@ -0,0 +1,32 @@
+package com.workintech.s18d1.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@AllArgsConstructor
+@Entity
+@Data
+@Table(name="burger",schema="fsweb")
+public class Burger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long;
+
+     @Column(name="name",nullable = false)
+    private String name;
+
+     @Column(name="price",nullable = false)
+    private Double price;
+
+     @Column(name="is_vegan")
+    private Boolean isVegan;
+
+     @Column(name= "bread_type")
+     @Enumerated(EnumType.STRING)
+     private BreadType breadType;
+
+
+     @Column(name="contents")
+     private String contents;
+
+}
